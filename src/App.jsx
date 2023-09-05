624617c76524744eba93a736ca725f660458605d
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-09-06 07:28:37 +0800

    Implement get current chapter text
    - Add getCfiBase to get the current chapter from the epubcif a code that contains the information for the current chapter being rendered.
    
    - Add getCurrentChapterText to iterate  through the book and find the html content with the current chapter and returns it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,67 @@
-import { useState } from "react";
-import { ReactReader, ReactReaderStyle } from "react-reader";
-import "./custom-reader-styles.css"; // Replace with the actual path
+import { useState, useEffect, useRef } from "react";
+import { ReactReader } from "react-reader";
 import FileReaderInput from "react-file-reader-input";
-
-const ownStyles = {
-  ...ReactReaderStyle,
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    color: "red",
-  },
-};
+import "./custom-reader-styles.css"; // Replace with the actual path
 
 const App = () => {
-  const locationChanged = (epubcifi) => {
-    setLocation(epubcifi);
-  };
-
   const [location, setLocation] = useState(
     localStorage.getItem("epub-location") || 2
   );
   const [localFile, setLocalFile] = useState(null);
   const [localName, setLocalName] = useState(null);
+  const renditionRef = useRef(null);
 
   const defaultUrl = "https://react-reader.metabits.no/files/alice.epub"; // Replace with your default URL
+
+  const getCfiBase = (epubcifi) => {
+    // Sample epubcifi: epubcfi(/6/6!/4/2/4[pgepubid00001]/1:0)
+    // epubcifi is the status of the current rendered page in the epub
+
+    // We want to extract the substring /6/6, which I used as a basis for the chapter number
+    const match = epubcifi.match(/\/\d+\/\d+/);
+
+    if (match) {
+      const extractedSubstring = match[0];
+      return extractedSubstring;
+    } else {
+      return null;
+    }
+  };
+  const loadBook = (renditionRef) => {
+    if (renditionRef.current) {
+      const book = renditionRef.current.book;
+      return book;
+    } else {
+      return null;
+    }
+  };
+
+  const getCurrentChapterText = async (book) => {
+    const bookSpine = await book.loaded.spine;
+
+    return new Promise((resolve, reject) => {
+      bookSpine.each(async (section) => {
+        if (section.cfiBase === getCfiBase(location)) {
+          try {
+            const contents = await section.load(book.load.bind(book));
+            const bodyElement = contents.querySelector("body");
+            if (bodyElement) {
+              resolve(bodyElement.textContent);
+            } else {
+              reject(new Error("No body element found in the chapter."));
+            }
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+    });
+  };
+
+  const locationChanged = (epubcifi) => {
+    setLocation(epubcifi);
+    console.log("locationChanged: epubcifi: ", getCfiBase(epubcifi));
+  };
 
   const handleChangeFile = (event, results) => {
     if (results.length > 0) {
@@ -36,6 +75,21 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    // Access the book object
+    const book = loadBook(renditionRef);
+
+    if (book) {
+      getCurrentChapterText(book)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [location]);
+
   return (
     <div className="">
       <FileReaderInput as="buffer" onChange={handleChangeFile}>
@@ -46,6 +100,9 @@ const App = () => {
           location={location}
           locationChanged={locationChanged}
           url={localFile || defaultUrl} // Use localFile or the default URL
+          getRendition={(rendition) => {
+            renditionRef.current = rendition;
+          }}
         />
       </div>
     </div>
