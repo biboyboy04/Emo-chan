21a28b238b91a93a9a9beb418e90f7986e668b54
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-11-11 18:41:16 +0800

    Update main.jsx
    - Add App.jsx link path to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./components/HomePage.jsx";
+import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
@@ -8,6 +9,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/App",
+    element: <App />,
   },
 ]);
 
