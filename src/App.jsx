0819a62931180000b1cfe1ad28303a9948d4733b
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-11-11 19:52:04 +0800

    Refactor App.jsx
    - Get bookUrl from useNavigation
    
    - shorten some codes
    
    - Remove some comments
    
    - Remove file import because file import will be moved to HomePage.jsx. So App.jsx is only focusing on reading the book that was clicked or imported in HomePage.jsx
    
    - Made url into the selectedBook which is whatever book that was selected in  HomePage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@ import ReactLoading from "react-loading";
 
 const App = () => {
   const [location, setLocation] = useState(
-    localStorage.getItem("epub-location") || 2
+    localStorage.getItem("epub-location")
   );
-  const [localFile, setLocalFile] = useState(null);
-  const [localName, setLocalName] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [storyText, setStoryText] = useState("");
 
@@ -19,11 +17,7 @@ const App = () => {
   const renditionRef = useRef(null);
 
   const locationParam = useLocation();
-  console.log(locationParam, "locationParam");
-  const selectedBook = locationParam.state.bookId;
-  console.log(selectedBook, "selectedBook");
-
-  const defaultUrl = "/public/e-books/grimms.epub"; // Replace with your default URL
+  const selectedBook = locationParam.state.bookUrl;
 
   // Can be more shorter
   const getCfiChapter = (epubcifi) => {
@@ -41,16 +35,11 @@ const App = () => {
       }
     }
 
-    return null; // Return null if the parameter is not a string or doesn't match the pattern
+    return null;
   };
 
   const loadBook = (renditionRef) => {
-    if (renditionRef.current) {
-      const book = renditionRef.current.book;
-      return book;
-    } else {
-      return null;
-    }
+    return renditionRef.current ? renditionRef.current.book : null;
   };
 
   const getCurrentChapterText = async (book) => {
@@ -58,8 +47,6 @@ const App = () => {
 
     return new Promise((resolve, reject) => {
       bookSpine.each(async (section) => {
-        // Section == Chapter
-        // most ebook chapters starts at index 2
         if (section.index > 1 && section.cfiBase === getCfiChapter(location)) {
           try {
             const contents = await section.load(book.load.bind(book));
@@ -78,18 +65,6 @@ const App = () => {
     });
   };
 
-  const handleChangeFile = (event, results) => {
-    if (results.length > 0) {
-      const [e, file] = results[0];
-      if (file.type !== "application/epub+zip") {
-        return alert("Unsupported type. Please upload an epub file.");
-      }
-      setLocalFile(e.target.result);
-      setLocalName(file.name);
-      setLocation(null);
-    }
-  };
-
   useEffect(() => {
     // When the location changes, get and change the chapter
     const currentChapter = getCfiChapter(location);
@@ -100,12 +75,10 @@ const App = () => {
 
       // Access the book object
       const book = loadBook(renditionRef);
-      // double chech if redundant code : if book check
       if (book) {
         getCurrentChapterText(book)
           .then((result) => {
             setStoryText(result);
-            // console.log(result);
           })
           .catch((error) => {
             console.error(error);
@@ -129,15 +102,11 @@ const App = () => {
       )}
       <SpotifyPlaylist storyText={storyText} setIsLoading={setIsLoading} />
 
-      <FileReaderInput as="buffer" onChange={handleChangeFile}>
-        <button>Upload local epub</button>
-      </FileReaderInput>
-
       <div className="reader-container">
         <ReactReader
           location={location}
           locationChanged={setLocation}
-          url={localFile || selectedBook}
+          url={selectedBook}
           getRendition={(rendition) => {
             renditionRef.current = rendition;
           }}
