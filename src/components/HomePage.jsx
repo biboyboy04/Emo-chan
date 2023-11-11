fb5cb3bd221821d29e8239c8b58efcba9893d7e1
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-11-11 18:37:32 +0800

    Update HomePage.jsx
    - Add book props
    
    - Dynamically generate books, based on props
    
    - Add useNavigate to redirect to the App.jsx
    
    - Add handleBookClick to pass the url of the book to the App.jsx when navigating so that the book can be read by the ReactReader

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,67 @@
-// HomePage.js
-
 import React from "react";
+import FileReaderInput from "react-file-reader-input";
+import { useState, useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Reader from "../Reader";
 
 const HomePage = () => {
+  const [location, setLocation] = useState(
+    localStorage.getItem("epub-location") || 2
+  );
+  const [localFile, setLocalFile] = useState(null);
+  const [localName, setLocalName] = useState(null);
+  const navigate = useNavigate();
+  const books = [
+    {
+      id: 1,
+      title: "Alice's Adventures in Wonderland",
+      author: "Lewis Carroll",
+      genre: "Fiction",
+      cover: "/public/e-books/alice.jpg",
+      url: "/public/e-books/alice.epub",
+    },
+    {
+      id: 2,
+      title: "Grimms' Fairy Tales",
+      author: "Jacob Grimm and Wilhelm Grimm",
+      genre: "Non-fiction",
+      cover: "/public/e-books/grimms.png",
+      url: "/public/e-books/grimms.epub",
+    },
+    {
+      id: 3,
+      title: "The Happy Prince and Other Tales",
+      author: "Oscar Wilde",
+      genre: "Mystery",
+      cover: "/public/e-books/happy_prince.jpg",
+      url: "/public/e-books/happy_prince.epub",
+    },
+    {
+      id: 4,
+      title: "The Jungle Book",
+      author: "Rudyard Kipling",
+      genre: "Mystery",
+      cover: "/public/e-books/jungle_book.jpg",
+      url: "/public/e-books/jungle_book.epub",
+    },
+  ];
+
+  const handleChangeFile = (event, results) => {
+    if (results.length > 0) {
+      const [e, file] = results[0];
+      if (file.type !== "application/epub+zip") {
+        return alert("Unsupported type. Please upload an epub file.");
+      }
+      setLocalFile(e.target.result);
+      setLocalName(file.name);
+      setLocation(null);
+    }
+  };
+
+  const handleBookClick = (bookUrl) => {
+    navigate("/App", { state: { bookUrl } });
+  };
+
   return (
     <div className="home-page">
       <div className="navbar-header">
@@ -10,40 +69,33 @@ const HomePage = () => {
         <div className="hamburger">X</div>
       </div>
       <div className="search-and-add">
-        <input type="text" placeholder="Search for books" />
-        <button>Add Book</button>
+        <FileReaderInput as="buffer" onChange={handleChangeFile}>
+          <button>Upload local epub</button>
+        </FileReaderInput>
       </div>
-      {/* Display the imported or default books here */}
       <div className="book-list">
-        {/* Example book item */}
-        <div className="book">
-          <img src="book-cover.jpg" alt="Book Title" />
-          <div className="book-details">
-            <h3>Book Title</h3>
-            <p>Author: Author Name</p>
-            <p>Genre: Fiction</p>
+        {books.map((book) => (
+          <div
+            className="book"
+            key={book.id}
+            onClick={() => handleBookClick(book.url)}
+          >
+            <img src={`${book.cover}`} alt={book.title} />
+            <div className="book-details">
+              <h3>{book.title}</h3>
+              <p>Author: {book.author}</p>
+              <p>Genre: {book.genre}</p>
+            </div>
           </div>
-        </div>
-        <div className="book">
-          <img src="book-cover.jpg" alt="Book Title" />
-          <div className="book-details">
-            <h3>Book Title</h3>
-            <p>Author: Author Name</p>
-            <p>Genre: Fiction</p>
-          </div>
-        </div>
-        <div className="book">
-          <img src="book-cover.jpg" alt="Book Title" />
-          <div className="book-details">
-            <h3>Book Title</h3>
-            <p>Author: Author Name</p>
-            <p>Genre: Fiction</p>
-          </div>
-        </div>
-        {/* Repeat this structure for each book */}
+        ))}
       </div>
     </div>
   );
 };
+// This is for passing props onlyu
+// Router
+// Props to the book
+// Add logicv for import
+// All book related function oncluding spotifty is not here
 
 export default HomePage;
