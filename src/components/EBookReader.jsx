cdb312bd5c06ac7413e45ba517142ecbd4eccb65
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-11-27 23:35:49 +0800

    Isolate readerStyles from EBookReader.jsx
    - Create reactReaderStyles.js and transfer the style from the EBookReader.jsx.
    
    - Export the readerStyles to be use in EBookReader.jsx.

--- src/components/EBookReader.jsx
+++ src/components/EBookReader.jsx
@@ -1,81 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
-import { ReactReader, ReactReaderStyle } from "react-reader";
+import { ReactReader } from "react-reader";
 import SpotifyPlaylist from "./spotifyPlaylist";
 import ReactLoading from "react-loading";
 import { useMediaQuery } from "react-responsive";
 import Navbar from "./Navbar";
-
-const readerStyles = {
-  ...ReactReaderStyle,
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    backgroundColor: "white",
-    color: "black",
-    fontWeight: 100,
-    fontFamily: "Playpen Sans",
-  },
-  tocArea: {
-    ...ReactReaderStyle.tocArea,
-    backgroundColor: "white",
-    color: "black",
-  },
-  tocAreaButton: {
-    ...ReactReaderStyle.tocAreaButton,
-    backgroundColor: "white",
-    color: "black",
-    fontFamily: "Playpen Sans",
-  },
-  tocButtonBar: {
-    ...ReactReaderStyle.tocButtonBar,
-    backgroundColor: "black",
-  },
-  tocButton: {
-    ...ReactReaderStyle.tocButton,
-    backgroundColor: "white",
-    color: "black",
-  },
-  tocButtonExpanded: {
-    ...ReactReaderStyle.tocButtonExpanded,
-    backgroundColor: "white",
-  },
-};
-
-const readerStylesMobile = {
-  ...readerStyles,
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    backgroundColor: "white",
-    color: "black",
-    fontWeight: 300,
-    fontFamily: "Playpen Sans",
-    fontSize: "2rem",
-    padding: "0px",
-    marginTop: "-10px",
-    zIndex: "100",
-  },
-  next: {
-    ...ReactReaderStyle.next,
-    top: "10px",
-    right: "10px",
-  },
-  prev: {
-    ...ReactReaderStyle.prev,
-    top: "10px",
-    right: "40px",
-    left: "auto",
-  },
-  reader: {
-    ...ReactReaderStyle.reader,
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "100%",
-    height: "calc(100% - 50px)",
-    marginTop: "20px",
-  },
-};
+import { readerStyles, readerStylesMobile } from "../reactReaderStyles";
 
 const EBookReader = () => {
   const [location, setLocation] = useState(
