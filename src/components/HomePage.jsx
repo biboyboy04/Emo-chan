578cfccc57306d903db70864bbc7a76ac0dff6f2
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2023-11-27 23:28:41 +0800

    Separate the book object in HomePage.jsx
    - Create a new js file book-details, with the information from the previous book object
    
    - Import book-details.js to the HomePage to use as a replacement.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,44 +1,7 @@
 import FileReaderInput from "react-file-reader-input";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
-
-const books = [
-  {
-    id: 1,
-    title: "Alice's Adventures in Wonderland",
-    author: "Lewis Carroll",
-    cover: "e-books/alice.jpg",
-    url: "e-books/alice.epub",
-  },
-  {
-    id: 2,
-    title: "Grimms' Fairy Tales",
-    author: "Jacob Grimm and Wilhelm Grimm",
-    cover: "e-books/grimms.png",
-    url: "e-books/grimms.epub",
-  },
-  {
-    id: 3,
-    title: "Omniscient Reader's Viewpoint",
-    author: "Singshong",
-    cover: "e-books/ovr.jpg",
-    url: "e-books/ovr.epub",
-  },
-  {
-    id: 4,
-    title: "The Jungle Book",
-    author: "Rudyard Kipling",
-    cover: "e-books/jungle_book.jpg",
-    url: "e-books/jungle_book.epub",
-  },
-  // {
-  //   id: 3,
-  //   title: "The Happy Prince and Other Tales",
-  //   author: "Oscar Wilde",
-  //   cover: "e-books/happy_prince.jpg",
-  //   url: "e-books/happy_prince.epub",
-  // },
-];
+import books from "../book-details.js";
 
 const HomePage = () => {
   const navigate = useNavigate();
